2c1e945209cd7f92c192bacc4c050b73944aa553
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2026-02-04 09:12:26 +0600

    add SSLCommerz integration with configuration, controller, route, and service files
    Signed-off-by: kazimuntasirrahman <kazimuntasirrahman@gmail.com>

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,6 +7,11 @@ const config = {
   neon_conn_str: process.env.CONNECTION_STRING,
   port: process.env.PORT || 5000,
   jwt_secret: process.env.JWT_SECRET,
+  sslcommerz: {
+    store_id: process.env.SSLCOMMERZ_STORE_ID,
+    store_password: process.env.SSLCOMMERZ_STORE_PASSWORD,
+    base_url: process.env.SSLCOMMERZ_BASE_URL,
+  },
 };
 
 export default config;
